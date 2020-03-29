72f5601cffe46187bd7ac8cf5e6c9ccd247cacd8
nA-div-B-equ-kB-kER <tishko99a@gmail.com>
2020-03-29 17:24:59 -0400

    Add files via upload

---
+++ Assests/Scripts/MenuBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    
+    public void triggerMenuBehav(bool i){
+        if(i){
+            Application.Quit();
+        }
+        else{
+            SceneManager.LoadScene("MiniGame1");
+        }
+    }
+    
+}
